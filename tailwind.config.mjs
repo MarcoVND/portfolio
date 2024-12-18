49ef08f3d236a28289eeaf505d72c0eefff61b07
MarcoVNDev <marcovndev@gmail.com>
2024-12-18 07:25:13 -0600

    New UI

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,48 +5,7 @@ export default {
     extend: {},
   },
   daisyui: {
-    themes: [
-      {
-        dark: {
-          primary: "#fca311",
-
-          secondary: "#1c1b23",
-
-          accent: "#0099FF",
-
-          neutral: "#ff00ff",
-
-          "base-100": "#1c1b23",
-
-          info: "#0000ff",
-
-          success: "#00ff00",
-
-          warning: "#00ff00",
-
-          error: "#ff0000",
-        },
-        light: {
-          primary: "#fca311",
-
-          secondary: "#14213d",
-
-          accent: "#0099FF",
-
-          neutral: "#ff00ff",
-
-          "base-100": "#fff",
-
-          info: "#0000ff",
-
-          success: "#00ff00",
-
-          warning: "#00ff00",
-
-          error: "#ff0000",
-        },
-      },
-    ],
+    themes: ["light", "dark"],
   },
   plugins: [require("daisyui")],
 };
